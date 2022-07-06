825f301330599d181155d0ab1b566d0403cc7724
maxime <61885525+madaumur@users.noreply.github.com>
2022-07-06 21:22:27 +0200

    + TrierMots

---
+++ JAVA-TrieMots/src/com/afpa/Main.java
@@ -0,0 +1,26 @@
+package com.afpa;
+
+import java.util.Arrays;
+
+import static com.afpa.Outils.*;
+
+public
+class Main
+{
+    public static
+    void main( String[] args )
+    {
+        int      arraySize     = askArraySize();
+        String[] tableauString = new String[ arraySize ];
+
+        for ( int count = 0 ; count < arraySize ; count++ )
+        {
+            tableauString[ count ] = questionWindow( String.format( "Entrer le mot n°%d%n", count + 1 ),
+                                                     "demande_String" ).trim();
+        }
+
+        informativeWindow( tabToString( tableauString ), "affichage_Tableau" );
+        Arrays.sort( tableauString );
+        informativeWindow( tabToString( tableauString ), "affichage_Tableau" );
+    }
+}
